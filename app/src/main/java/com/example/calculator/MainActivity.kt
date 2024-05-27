d7b164e82fcbb096075a80bc1f1a13bc87f12206
İdil Öncü <oncuidil@gmail.com>
2024-05-28 00:55:12 +0300

    methods are completed

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -10,5 +10,33 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.mainActivityObject=this
+
+        binding.answerResult="0"
+
+    }
+    fun buttonAddClicked(enteredNum1:String,enteredNum2:String){
+        binding.buttonAdd.setOnClickListener{
+            val add=enteredNum1.toInt() + enteredNum2.toInt()
+            binding.answerResult = add.toString()
+        }
+    }
+    fun buttonMultipleClicked(enteredNum1:String,enteredNum2:String){
+        binding.buttonMultiple.setOnClickListener{
+            val mul=enteredNum1.toInt() * enteredNum2.toInt()
+            binding.answerResult = mul.toString()
+        }
+    }
+    fun buttonSubClicked(enteredNum1:String,enteredNum2:String){
+        binding.buttonSub.setOnClickListener{
+            val sub=enteredNum1.toInt() - enteredNum2.toInt()
+            binding.answerResult = sub.toString()
+        }
+    }
+    fun buttonDividedClicked(enteredNum1:String,enteredNum2:String){
+        binding.buttonDivided.setOnClickListener{
+            val div=enteredNum1.toInt() / enteredNum2.toInt()
+            binding.answerResult= div.toString()
+        }
     }
 }
